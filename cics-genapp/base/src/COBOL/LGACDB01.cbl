b108462d2222c0d97824d8bac3d2b30ffcc3ab1e
132464866 <132464866@9.171.203.182>
2023-03-07 14:31:31 +0000

    testing impact

--- cics-genapp/base/src/COBOL/LGACDB01.cbl
+++ cics-genapp/base/src/COBOL/LGACDB01.cbl
@@ -148,7 +148,7 @@
       * Common code                                                    *
       *----------------------------------------------------------------*
       * initialize working storage variables
-           DISPLAY 'impact build 8'.
+           DISPLAY 'impact build 9'.
            INITIALIZE WS-HEADER.
       * set up general variable
            MOVE EIBTRNID TO WS-TRANSID.
